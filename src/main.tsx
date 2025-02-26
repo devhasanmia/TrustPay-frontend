0c6800d9bff92bc4e08775c49172661962b18626
devhasanmia <dev.hasanmia@gmail.com>
2025-02-26 20:56:48 +0600

    added redux rtk

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router'
 import router from './router'
+import { Provider } from 'react-redux'
+import { store } from './redux/store'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <Provider store={store}>
+   <StrictMode>
   <RouterProvider router={router} />
-  </StrictMode>,
+  </StrictMode>
+  </Provider>
 )
---
+++ src/redux/api/baseApi.ts
@@ -0,0 +1,9 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { config } from "../../config";
+
+export const baseApi = createApi({
+  reducerPath: "baseApi",
+  baseQuery: fetchBaseQuery({ baseUrl: config.trustPayApiUrl}),
+  endpoints: () => ({}),
+});
+
